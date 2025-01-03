70a976585d358815688389585d2c8b88ee472c10
munir <smsirajulmonir@gmail.com>
2025-01-03 15:00:54 +0600

    dark mode toggle implemented

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,13 @@
-import { Button } from "./components/ui/button";
-import { decrement, increment } from "./redux/features/counter/counterSlice";
-import { useAppDispatch, useAppSelector } from "./redux/hook";
+import Navbar from "./components/layout/Navbar";
+
+import { Outlet } from "react-router-dom";
 
 function App() {
-  const dispatch = useAppDispatch();
-  const { count } = useAppSelector((state) =>state.counter)
-
-
-  const handleIncrement =(amount:number)=>{
-    dispatch(increment({amount:amount}));
-  };
-  const handleDecrement =()=>{
-    dispatch(decrement())
-  }
-
+ 
   return (
    <div>
-      <h1>Counter with Redux</h1>
-      <Button onClick={()=>handleIncrement(5)}>Increment by 5</Button>
-      <Button onClick={()=>handleIncrement(1)}>Increment</Button>
-      <div>{count}</div>
-      <Button onClick={handleDecrement}>Decrement</Button>
+      <Navbar/>
+      <Outlet />
    </div>
   )
 }
